0681c5464cf58b24a1242bb0e038754a404c343e
Matheus Warmeling Matias <matheuswarmeling@hotmail.com>
2018-12-10 09:10:45 -0200

    Adicionado conexão https e parâmetro para dias de pesquisa

--- src/main/java/com/github/alexsandrospecht/ws/ReceitaWS.java
+++ src/main/java/com/github/alexsandrospecht/ws/ReceitaWS.java
@@ -13,16 +13,16 @@ public interface ReceitaWS {
     @RequestLine("GET /v1/cnpj/{cnpj}")
     String consulta(@Param("cnpj") String cnpj);
 
-
     @RequestLine("GET /v1/cnpj/{cnpj}")
-    String consulta(@Param("cnpj") String cnpj, @Param("token") String token);
+    @Headers("Authorization: {token}")
+    String consulta(@Param("cnpj") String cnpj, @Param("token") String token, @Param("days") Integer days);
 
     @RequestLine("GET /v1/cnpj/{cnpj}")
     @Headers("Authorization: {token}")
     RetornoWrapper consultaWrapper(@Param("cnpj") String cnpj);
 
-    @RequestLine("GET /v1/cnpj/{cnpj}")
+    @RequestLine("GET /v1/cnpj/{cnpj}/days/{days}")
     @Headers("Authorization: {token}")
-    RetornoWrapper consultaWrapper(@Param("cnpj") String cnpj, @Param("token") String token);
+    RetornoWrapper consultaWrapper(@Param("cnpj") String cnpj, @Param("token") String token, @Param("days") Integer days);
 
 }
